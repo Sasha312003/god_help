6ec5cbd0c0642060a83d628ad1af9a565f282674
alex <rubikon488@gmail.com>
2022-09-16 13:04:21 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_phone_auth_handler/firebase_phone_auth_handler.dart';
+import 'package:flutter/material.dart';
+import 'package:god_help/firebase_options.dart';
+import 'package:god_help/screens/splash_screen.dart';
+import 'package:god_help/utils/app_theme.dart';
+import 'package:god_help/utils/globals.dart';
+import 'package:god_help/utils/route_generator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FirebasePhoneAuthProvider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'FirebasePhoneAuthHandler Demo',
+        scaffoldMessengerKey: Globals.scaffoldMessengerKey,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        onGenerateRoute: RouteGenerator.generateRoute,
+        initialRoute: SplashScreen.id,
+      ),
+    );
+  }
+}
